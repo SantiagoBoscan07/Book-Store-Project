ff91480718f949e8c65ab2b0d7a2bd56ede87a13
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2025-11-22 13:59:30 -0500

    Project Setup

---
+++ GroupProject/frmMainMenu.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace GroupProject
+{
+    partial class frmMainMenu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            lblInfo = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(lblInfo);
+            panel1.Location = new Point(12, 85);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(482, 264);
+            panel1.TabIndex = 0;
+            // 
+            // lblInfo
+            // 
+            lblInfo.AutoSize = true;
+            lblInfo.Location = new Point(15, 13);
+            lblInfo.Name = "lblInfo";
+            lblInfo.Size = new Size(38, 15);
+            lblInfo.TabIndex = 0;
+            lblInfo.Text = "label1";
+            // 
+            // frmMainMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "frmMainMenu";
+            Text = "BookStore - MainMenu";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label lblInfo;
+    }
+}
---
+++ GroupProject/frmMainMenu.cs
@@ -0,0 +1,49 @@
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace GroupProject
+{
+    public partial class frmMainMenu : Form
+    {
+        public frmMainMenu()
+        {
+            InitializeComponent();
+            this.Load += frmMainMenu_Load;
+        }
+
+        public void frmMainMenu_Load(object sender, EventArgs e)
+        {
+            string connString = ConfigurationManager
+                .ConnectionStrings["GroupProject.Properties.Settings.BookStoreDBConnectionString"]
+                .ConnectionString;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    // ADO.NET command to query system tables for all user tables
+                    string query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        string output = "Tables:\n";
+
+                        while (reader.Read())
+                        {
+                            output += reader.GetString(0) + "\n";
+                        }
+
+                        lblInfo.Text = output;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblInfo.Text = "Error: " + ex.Message;
+            }
+        }
+    }
+}
