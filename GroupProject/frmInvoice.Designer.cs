da88689b76d78010ebbc22c47c17a3852b8b5772
mariahsharris <mariahsharris2017@gmail.com>
2025-11-23 21:24:22 -0500

    Add files via upload

---
+++ GroupProject/frmInvoice.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace GroupProject
+{
+    partial class frmInvoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            lblInvoice = new Label();
+            lblOrderID = new Label();
+            txtOrderID = new TextBox();
+            btnSearch = new Button();
+            dgvInvoiceItems = new DataGridView();
+            colTitle = new DataGridViewTextBoxColumn();
+            colQuantity = new DataGridViewTextBoxColumn();
+            colPrice = new DataGridViewTextBoxColumn();
+            colExtendedPrice = new DataGridViewTextBoxColumn();
+            lblSubtotal = new Label();
+            lblTax = new Label();
+            lblTotal = new Label();
+            txtSubtotal = new TextBox();
+            txtTax = new TextBox();
+            txtTotal = new TextBox();
+            btnClose = new Button();
+            btnPrint = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvInvoiceItems).BeginInit();
+            SuspendLayout();
+            // 
+            // lblInvoice
+            // 
+            lblInvoice.AutoSize = true;
+            lblInvoice.Location = new Point(37, 19);
+            lblInvoice.Name = "lblInvoice";
+            lblInvoice.Size = new Size(56, 20);
+            lblInvoice.TabIndex = 0;
+            lblInvoice.Text = "Invoice";
+            // 
+            // lblOrderID
+            // 
+            lblOrderID.AutoSize = true;
+            lblOrderID.Location = new Point(37, 65);
+            lblOrderID.Name = "lblOrderID";
+            lblOrderID.Size = new Size(69, 20);
+            lblOrderID.TabIndex = 1;
+            lblOrderID.Text = "Order ID:";
+            // 
+            // txtOrderID
+            // 
+            txtOrderID.Location = new Point(123, 62);
+            txtOrderID.Name = "txtOrderID";
+            txtOrderID.Size = new Size(125, 27);
+            txtOrderID.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(268, 62);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(135, 27);
+            btnSearch.TabIndex = 3;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // dgvInvoiceItems
+            // 
+            dgvInvoiceItems.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvInvoiceItems.Columns.AddRange(new DataGridViewColumn[] { colTitle, colQuantity, colPrice, colExtendedPrice });
+            dgvInvoiceItems.Location = new Point(37, 111);
+            dgvInvoiceItems.Name = "dgvInvoiceItems";
+            dgvInvoiceItems.RowHeadersWidth = 51;
+            dgvInvoiceItems.Size = new Size(738, 188);
+            dgvInvoiceItems.TabIndex = 4;
+            // 
+            // colTitle
+            // 
+            colTitle.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleRight;
+            colTitle.DefaultCellStyle = dataGridViewCellStyle1;
+            colTitle.HeaderText = "Title";
+            colTitle.MinimumWidth = 6;
+            colTitle.Name = "colTitle";
+            // 
+            // colQuantity
+            // 
+            colQuantity.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleRight;
+            colQuantity.DefaultCellStyle = dataGridViewCellStyle2;
+            colQuantity.HeaderText = "Quantity";
+            colQuantity.MinimumWidth = 6;
+            colQuantity.Name = "colQuantity";
+            // 
+            // colPrice
+            // 
+            colPrice.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleRight;
+            colPrice.DefaultCellStyle = dataGridViewCellStyle3;
+            colPrice.HeaderText = "Price";
+            colPrice.MinimumWidth = 6;
+            colPrice.Name = "colPrice";
+            // 
+            // colExtendedPrice
+            // 
+            colExtendedPrice.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleRight;
+            colExtendedPrice.DefaultCellStyle = dataGridViewCellStyle4;
+            colExtendedPrice.HeaderText = "Extended Price";
+            colExtendedPrice.MinimumWidth = 6;
+            colExtendedPrice.Name = "colExtendedPrice";
+            // 
+            // lblSubtotal
+            // 
+            lblSubtotal.AutoSize = true;
+            lblSubtotal.Location = new Point(445, 333);
+            lblSubtotal.Name = "lblSubtotal";
+            lblSubtotal.Size = new Size(68, 20);
+            lblSubtotal.TabIndex = 5;
+            lblSubtotal.Text = "Subtotal:";
+            // 
+            // lblTax
+            // 
+            lblTax.AutoSize = true;
+            lblTax.Location = new Point(446, 368);
+            lblTax.Name = "lblTax";
+            lblTax.Size = new Size(67, 20);
+            lblTax.TabIndex = 6;
+            lblTax.Text = "Tax (6%):";
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Location = new Point(465, 404);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(45, 20);
+            lblTotal.TabIndex = 7;
+            lblTotal.Text = "Total:";
+            // 
+            // txtSubtotal
+            // 
+            txtSubtotal.Location = new Point(519, 326);
+            txtSubtotal.Name = "txtSubtotal";
+            txtSubtotal.ReadOnly = true;
+            txtSubtotal.Size = new Size(125, 27);
+            txtSubtotal.TabIndex = 8;
+            // 
+            // txtTax
+            // 
+            txtTax.Location = new Point(519, 365);
+            txtTax.Name = "txtTax";
+            txtTax.ReadOnly = true;
+            txtTax.Size = new Size(125, 27);
+            txtTax.TabIndex = 9;
+            // 
+            // txtTotal
+            // 
+            txtTotal.Location = new Point(519, 398);
+            txtTotal.Name = "txtTotal";
+            txtTotal.ReadOnly = true;
+            txtTotal.Size = new Size(125, 27);
+            txtTotal.TabIndex = 10;
+            // 
+            // btnClose
+            // 
+            btnClose.Location = new Point(195, 326);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(135, 29);
+            btnClose.TabIndex = 11;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // btnPrint
+            // 
+            btnPrint.Location = new Point(37, 326);
+            btnPrint.Name = "btnPrint";
+            btnPrint.Size = new Size(135, 29);
+            btnPrint.TabIndex = 12;
+            btnPrint.Text = "Print Invoice";
+            btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // frmInvoice
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnPrint);
+            Controls.Add(btnClose);
+            Controls.Add(txtTotal);
+            Controls.Add(txtTax);
+            Controls.Add(txtSubtotal);
+            Controls.Add(lblTotal);
+            Controls.Add(lblTax);
+            Controls.Add(lblSubtotal);
+            Controls.Add(dgvInvoiceItems);
+            Controls.Add(btnSearch);
+            Controls.Add(txtOrderID);
+            Controls.Add(lblOrderID);
+            Controls.Add(lblInvoice);
+            Name = "frmInvoice";
+            Text = "frmInvoice";
+            ((System.ComponentModel.ISupportInitialize)dgvInvoiceItems).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblInvoice;
+        private Label lblOrderID;
+        private TextBox txtOrderID;
+        private Button btnSearch;
+        private DataGridView dgvInvoiceItems;
+        private Label lblSubtotal;
+        private Label lblTax;
+        private Label lblTotal;
+        private TextBox txtSubtotal;
+        private TextBox txtTax;
+        private TextBox txtTotal;
+        private Button btnClose;
+        private Button btnPrint;
+        private DataGridViewTextBoxColumn colTitle;
+        private DataGridViewTextBoxColumn colQuantity;
+        private DataGridViewTextBoxColumn colPrice;
+        private DataGridViewTextBoxColumn colExtendedPrice;
+    }
+}
---
+++ GroupProject/frmInvoice.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+
+namespace GroupProject
+{
+    public partial class frmInvoice : Form
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["GroupProject.Properties.Settings.BookStoreDBConnectionString"].ConnectionString;
+
+
+        private PrintDocument printDocument1 = new PrintDocument();
+
+        public frmInvoice()
+        {
+            InitializeComponent();
+            printDocument1.PrintPage += PrintDocument1_PrintPage;
+
+            btnSearch.Click += btnSearch_Click;
+            btnPrint.Click += btnPrint_Click;
+            btnClose.Click += btnClose_Click;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtOrderID.Text))
+            {
+                MessageBox.Show("Please enter an Order ID.");
+                return;
+            }
+
+            LoadInvoice();
+        }
+
+        private void LoadInvoice()
+        {
+            DataTable dt = new DataTable();
+
+            string query = @"
+                SELECT 
+                    T.Title,
+                    OI.Quantity,
+                    T.Price,
+                    (OI.Quantity * T.Price) AS ExtendedPrice
+                FROM OrderItems OI
+                JOIN Titles T ON OI.TitleID = T.TitleID
+                WHERE OI.OrderID = @OrderID";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@OrderID", txtOrderID.Text);
+
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+
+            if (dt.Rows.Count == 0)
+            {
+                MessageBox.Show("No invoice found for this Order ID.");
+                dgvInvoiceItems.DataSource = null;
+                ClearTotals();
+                return;
+            }
+
+            dgvInvoiceItems.DataSource = dt;
+            CalculateTotals(dt);
+        }
+
+        private void CalculateTotals(DataTable dt)
+        {
+            decimal subtotal = 0;
+
+            foreach (DataRow row in dt.Rows)
+            {
+                subtotal += Convert.ToDecimal(row["ExtendedPrice"]);
+            }
+
+            decimal tax = subtotal * 0.06m;
+            decimal total = subtotal + tax;
+
+            txtSubtotal.Text = subtotal.ToString("C");
+            txtTax.Text = tax.ToString("C");
+            txtTotal.Text = total.ToString("C");
+        }
+
+        private void ClearTotals()
+        {
+            txtSubtotal.Text = "";
+            txtTax.Text = "";
+            txtTotal.Text = "";
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            PrintPreviewDialog preview = new PrintPreviewDialog();
+            preview.Document = printDocument1;
+            preview.Width = 1200;
+            preview.Height = 800;
+            preview.ShowDialog();
+        }
+
+        private void PrintDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Font headerFont = new Font("Segoe UI", 18, FontStyle.Bold);
+            Font textFont = new Font("Segoe UI", 11);
+            int y = 40;
+
+            e.Graphics.DrawString("Bookstore Invoice", headerFont, Brushes.Black, 260, y);
+            y += 50;
+
+            e.Graphics.DrawString($"Order ID: {txtOrderID.Text}", textFont, Brushes.Black, 50, y);
+            y += 35;
+
+            e.Graphics.DrawString("Title", textFont, Brushes.Black, 50, y);
+            e.Graphics.DrawString("Qty", textFont, Brushes.Black, 300, y);
+            e.Graphics.DrawString("Price", textFont, Brushes.Black, 380, y);
+            e.Graphics.DrawString("Extended", textFont, Brushes.Black, 470, y);
+            y += 25;
+
+            e.Graphics.DrawLine(Pens.Black, 50, y, 750, y);
+            y += 10;
+
+            foreach (DataGridViewRow row in dgvInvoiceItems.Rows)
+            {
+                if (row.Cells[0].Value == null) continue;
+
+                e.Graphics.DrawString(row.Cells[0].Value.ToString(), textFont, Brushes.Black, 50, y);
+                e.Graphics.DrawString(row.Cells[1].Value.ToString(), textFont, Brushes.Black, 300, y);
+                e.Graphics.DrawString(Convert.ToDecimal(row.Cells[2].Value).ToString("C"), textFont, Brushes.Black, 380, y);
+                e.Graphics.DrawString(Convert.ToDecimal(row.Cells[3].Value).ToString("C"), textFont, Brushes.Black, 470, y);
+                y += 25;
+            }
+
+            y += 20;
+            e.Graphics.DrawLine(Pens.Black, 50, y, 750, y);
+            y += 20;
+
+            e.Graphics.DrawString($"Subtotal: {txtSubtotal.Text}", textFont, Brushes.Black, 500, y);
+            y += 25;
+            e.Graphics.DrawString($"Tax: {txtTax.Text}", textFont, Brushes.Black, 500, y);
+            y += 25;
+            e.Graphics.DrawString($"Total: {txtTotal.Text}", new Font("Segoe UI", 11, FontStyle.Bold), Brushes.Black, 500, y);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
+
