f3d1ee11234edda686d3d1783a00a91c0810c9e0
mariahsharris <mariahsharris2017@gmail.com>
2025-11-30 17:10:04 -0500

    Update OrderItem.cs

--- GroupProject/OrderItem.cs
+++ GroupProject/OrderItem.cs
@@ -1,9 +1,13 @@
 namespace GroupProject
 {
-    internal class OrderItem
+    public class OrderItem
     {
+        public int OrderItemID { get; set; }
         public int OrderID { get; set; }
-        public int TitleID { get; set; }
+        public int BookID { get; set; }
         public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal ExtendedPrice => Quantity * Price;
     }
 }
+
