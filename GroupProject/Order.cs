1595ed242054300aa97ac4008629fd5f6fbd215f
mariahsharris <mariahsharris2017@gmail.com>
2025-11-30 17:09:52 -0500

    Update Order.cs

--- GroupProject/Order.cs
+++ GroupProject/Order.cs
@@ -1,12 +1,11 @@
-using System;
-
 namespace GroupProject
 {
-    internal class Order
+    public class Order
     {
         public int OrderID { get; set; }
-        public int StoreID { get; set; }
-        public int EmployeeID { get; set; }
+        public int CustomerID { get; set; }
         public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
     }
 }
+
