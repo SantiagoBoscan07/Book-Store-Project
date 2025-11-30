7bfcedfef60307f9b3f8cb4fc1eda3296fe419b1
mariahsharris <mariahsharris2017@gmail.com>
2025-11-30 17:04:33 -0500

    Create OrderItem.cs

---
+++ GroupProject/OrderItem.cs
@@ -0,0 +1,9 @@
+namespace GroupProject
+{
+    internal class OrderItem
+    {
+        public int OrderID { get; set; }
+        public int TitleID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
