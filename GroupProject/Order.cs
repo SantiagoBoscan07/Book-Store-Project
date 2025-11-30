c7efe5fb1c52b7c3081144cc0a8113d9bf582781
mariahsharris <mariahsharris2017@gmail.com>
2025-11-30 17:03:54 -0500

    Create Order.cs

---
+++ GroupProject/Order.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GroupProject
+{
+    internal class Order
+    {
+        public int OrderID { get; set; }
+        public int StoreID { get; set; }
+        public int EmployeeID { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
